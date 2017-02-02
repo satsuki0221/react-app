9d5d651d78a66a62def1820facf78f4ff6a6fe46
satsuki0221 <ymmo.9m@gmail.com>
2017-02-02 22:54:36 +0900

    React 学習 + モダン

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './app/index.jsx'
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path:'./',
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015','react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css?modules'],
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
